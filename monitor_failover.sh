f37099b8c7aa5db6c01fca6585014d53191cd689
Sandra Olisama <sandraolisama555@gmail.com>
2025-11-04 12:12:29 +0100

    Update monitor_failover.sh

--- monitor_failover.sh
+++ monitor_failover.sh
@@ -7,7 +7,7 @@ for CONTAINER in "${CONTAINERS[@]}"; do
     STATUS=$(docker inspect -f '{{.State.Running}}' $CONTAINER 2>/dev/null)
     if [ "$STATUS" != "true" ]; then
         # Send Slack alert
-        ./notify_slack.sh "Failover 🚨 - $CONTAINER is down!"
+        ./notify_slack.sh "Failover - $CONTAINER is down!"
     fi
 done
 
